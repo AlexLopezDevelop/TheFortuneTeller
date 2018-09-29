4d68164e09ec109c7a5bc060332e221b99b3322c
Cristian Lopez <alex_lopez.es@icloud.com>
2018-09-29 02:21:43 +0200

    Finished

--- TheFortuneTeller/ViewController.swift
+++ TheFortuneTeller/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelAnswer: UILabel!
+    @IBOutlet weak var askButton: UIButton!
+    
+    let answersArray : [String] = ["Yes", "No", "Can be", "Ask again", "I have no idea", "Don`t even dream about it"]
+    let answrsArrayLength : UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.answrsArrayLength = UInt32(self.answersArray.count)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.labelAnswer.text = ""
+        
+        //Dynamically changing font size of UILabel
+        self.labelAnswer.numberOfLines = 1
+        self.labelAnswer.adjustsFontSizeToFitWidth = true
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        self.labelAnswer.text = self.answersArray[Int(arc4random_uniform(self.answrsArrayLength))]
+    }
+    
 }
 
