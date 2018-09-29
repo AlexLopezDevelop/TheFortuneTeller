f34577a187a34be01b0153337b91abb49453f16a
Cristian Lopez <alex_lopez.es@icloud.com>
2018-09-29 03:14:06 +0200

    Added shake option with song

--- TheFortuneTeller/ViewController.swift
+++ TheFortuneTeller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -15,6 +16,8 @@ class ViewController: UIViewController {
     
     let answersArray : [String] = ["Yes", "No", "Can be", "Ask again", "I have no idea", "Don`t even dream about it"]
     let answrsArrayLength : UInt32
+    
+    var songPlayer = AVAudioPlayer()
     
     required init?(coder aDecoder: NSCoder) {
         self.answrsArrayLength = UInt32(self.answersArray.count)
@@ -28,10 +31,58 @@ class ViewController: UIViewController {
         //Dynamically changing font size of UILabel
         self.labelAnswer.numberOfLines = 1
         self.labelAnswer.adjustsFontSizeToFitWidth = true
+        
+        prepareSongAndSession()
     }
 
     @IBAction func askButtonPressed(_ sender: Any) {
+        generateAnswer()
+    }
+    
+    //Shake implementation
+    override func becomeFirstResponder() -> Bool {
+        
+        return true
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            songPlayer.play()
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            songPlayer.stop()
+            generateAnswer()
+        }
+    }
+    
+    //Custom functions
+    func generateAnswer() {
         self.labelAnswer.text = self.answersArray[Int(arc4random_uniform(self.answrsArrayLength))]
+    }
+    
+    func prepareSongAndSession() {
+        
+        do {
+            
+            songPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "shake", ofType: "mp3")!))
+            songPlayer.prepareToPlay()
+            
+            do {
+                
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+                
+            } catch let sessionError {
+                print(sessionError)
+            }
+            
+        } catch let songPlayerError {
+            print(songPlayerError)
+        }
+        
     }
     
 }
